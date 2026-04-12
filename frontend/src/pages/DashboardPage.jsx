f32293a7f617a99c85185c1dac1bea2bd6d59914
Arsalan-Manso <2280332@szabist-isb.pk>
2026-04-12 14:57:54 +0500

    commit changes

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,7 +1,30 @@
-import DashboardLayout from "../components/DashboardLayout";
+import { Navigate, useParams } from "react-router-dom";
+import { roleFromParam } from "../constants/roles";
+import AdminDashboardPage from "./AdminDashboardPage";
+import DoctorDashboardPage from "./DoctorDashboardPage";
+import PatientDashboardPage from "./PatientDashboardPage";
+import PharmacyDashboardPage from "./PharmacyDashboardPage";
 
 const DashboardPage = () => {
-  return <DashboardLayout />;
+  const { role } = useParams();
+  const roleName = roleFromParam(role);
+
+  if (!roleName) {
+    return <Navigate to="/" replace />;
+  }
+
+  switch (roleName) {
+    case "Admin":
+      return <AdminDashboardPage />;
+    case "Doctor":
+      return <DoctorDashboardPage />;
+    case "Patient":
+      return <PatientDashboardPage />;
+    case "Pharmacy":
+      return <PharmacyDashboardPage />;
+    default:
+      return <Navigate to="/" replace />;
+  }
 };
 
 export default DashboardPage;
